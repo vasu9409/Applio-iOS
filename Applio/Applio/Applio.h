47baee5f9d0c4deb3483fb66d7a1a3dfc94d53e8
Vasu Savaliya <vasusavaliya0000@gmail.com>
2023-07-20 22:12:59 +0530

    Add Local Fremwork

---
+++ Applio/Applio/Applio.h
@@ -0,0 +1,18 @@
+//
+//  Applio.h
+//  Applio
+//
+//  Created by VASU SAVALIYA on 20/07/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Applio.
+FOUNDATION_EXPORT double ApplioVersionNumber;
+
+//! Project version string for Applio.
+FOUNDATION_EXPORT const unsigned char ApplioVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Applio/PublicHeader.h>
+
+
