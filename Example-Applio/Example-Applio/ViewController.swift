929fdbc0fd3d19bf4a49031f58927c716b04c32c
Vasu Savaliya <vasusavaliya0000@gmail.com>
2023-07-20 23:26:04 +0530

    changes

--- Example-Applio/Example-Applio/ViewController.swift
+++ Example-Applio/Example-Applio/ViewController.swift
@@ -6,12 +6,15 @@
 //
 
 import UIKit
+import Applio
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let tbl = UITableView()
+        tbl.registerNib(for: "")
     }
 
 
